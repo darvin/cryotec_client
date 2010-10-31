98a911e4b476457d21adbe0b868f118b206cccd3
Sergey Klimov <dcdarv@gmail.com>
2010-10-31 08:49:41 +0200

    makebat

--- makexe.bat
+++ makexe.bat
@@ -1,3 +1,5 @@
+git pull origin master
+git checkout FETCH_HEAD -f
 easy_install --upgrade qtdjango
 easy_install --upgrade cryotec_server
 python setup.py py2exe --includes sip,cryotec_server.machines.models,cryotec_server.actions.models,cryotec_server.actiontemplates.models,cryotec_server.clients.models,cryotec_server.checklists.models
